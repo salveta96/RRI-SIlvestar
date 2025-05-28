25bc36f79483b0916414a980901a2a4c972a2104
salveta96 <96339794+salveta96@users.noreply.github.com>
2025-05-28 17:58:48 +0200

    Add files via upload

---
+++ Lab4/GunBehaviour.cs
@@ -0,0 +1,52 @@
+using NUnit.Framework.Internal.Builders;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GunBehaviour : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private AudioSource _gunshotSound;
+    [SerializeField] private float _fireRate = 5f;
+
+    private float _nextFireTime = 0f;
+    private PlayerInput _playerInput;
+    private InputAction _fireAction;
+
+    private void Awake()
+    {
+        if (_animator == null)
+            Debug.LogWarning($"{nameof(GunBehaviour)}: Animator reference is missing.", this);
+        if (_particleSystem == null)
+            Debug.LogWarning($"{nameof(GunBehaviour)}: ParticleSystem reference is missing.", this);
+        if (_gunshotSound == null)
+            Debug.LogWarning($"{nameof(GunBehaviour)}: AudioSource reference is missing.", this);
+
+        _playerInput = GetComponent<PlayerInput>();
+        if (_playerInput != null)
+        {
+            _fireAction = _playerInput.actions["Fire"];
+        }
+    }
+
+    private void Update()
+    {
+        if (_fireAction != null && _fireAction.IsPressed() && Time.time >= _nextFireTime)
+        {
+            Fire();
+            _nextFireTime = Time.time + 1f / _fireRate;
+        }
+    }
+
+    private void Fire()
+    {
+        if (_animator != null)
+            _animator.SetTrigger("TrShoot");
+
+        if (_particleSystem != null)
+            _particleSystem.Play();
+
+        if (_gunshotSound != null)
+            _gunshotSound.Play();
+    }
+}
